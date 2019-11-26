4331231cd8d8568f669ecb8f436d88f3ffad99ea
Stefan Lang <Stefan.Lang@med.lu.se>
2019-11-26 16:00:56 +0100

    Working, but not compiling to exe

---
+++ MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Mono_dense2sparse
+{
+    public class MyClass
+    {
+        public string ReturnMessage()
+        {
+            return "Happy coding!";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Mono_dense2sparse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if ( args.Length != 2 ) {
+                Console.WriteLine("usage: \nMono_dense2sparse.exe <denseMatrix> <outputPrefix>");
+                return;
+            }
+
+            var PATH = args[0];
+            var OPATH = args[1];
+            System.IO.StreamReader file = File.OpenText( PATH );
+            string line= null;
+            StreamWriter Genefile = File.CreateText(OPATH + "Genes.txt");
+            StreamWriter Mfile = File.CreateText( OPATH+"Matrix.txt" );
+            var i = 0;
+            Mfile.WriteLine("rowID" + " " + "colID" + " " + "Value");
+            while( ( line = file.ReadLine()) != null) {
+                string[] Xval = line.Split("\t", StringSplitOptions.None);
+                if (i == 0)
+                {
+                    //public static System.IO.StreamReader Cellfile (OPATH+"Cells.txt" );
+                    File.WriteAllLines(OPATH + "Cells.txt",
+                                       Xval.Skip(1).ToArray());
+                }
+                else
+                {
+                    Genefile.WriteLine(Xval[0]);
+
+                    for (int a = 1; a < Xval.Length; a++)
+                    {
+                        if ( Int32.Parse(Xval[a]) != 0 ){
+                            Mfile.WriteLine(i + " " + a + " " + Xval[a]);
+                        }
+                    }
+                }
+                i ++;
+            }
+            Console.WriteLine("Hello World! sould have produced the files in data/Sparse");
+        }
+    }
+}
